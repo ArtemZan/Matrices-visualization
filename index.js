fbbd9a4c0be83038b4d3f1f05f92d39f8c0fda9f
ArtemZan <arttema9@gmail.com>
2021-04-04 13:28:54 +0300

    More matrices

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ var mouse_down = false;
 window.onmousedown = () => mouse_down = true;
 window.onmouseup = () => mouse_down = false;
 
+var relToCenter = vec => new vec2(vec.x - window.innerWidth / 2, window.innerHeight / 2 - vec.y);
+var relToWindow = vec => new vec2(vec.x + window.innerWidth / 2, window.innerHeight / 2 - vec.y);
+
 class Matrix2x2 {
     constructor(a, b, c, d) {
         this.a = a;
@@ -30,23 +33,69 @@ class vec2 {
     multiply(matrix2x2) {
         if (matrix2x2 instanceof Matrix2x2) {
             var res = new vec2;
-            // Object.assign(res, this);
-            // res.x -= window.innerWidth / 2;
-            // res.y = window.innerHeight / 2 - res.y;
             res.x = matrix2x2.a * this.x + matrix2x2.b * this.y;
             res.y = matrix2x2.c * this.x + matrix2x2.d * this.y;
-            // res.x += window.innerWidth / 2;
-            // res.y = window.innerHeight / 2 - res.y;
             return res;
         }
     }
+
+    rotate(rad) {
+        return multiply(new Matrix2x2(Math.cos(rad), -Math.sin(rad), Math.sin(rad), Math.cos(rad)));
+    }
+
+    normalize() {
+        let l = Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+        return new vec2(this.x / l, this.y / l);
+    }
+}
+
+function multiply(vertices, matrix) {
+    var res = []
+    vertices.forEach(vert => res.push(vert.multiply(matrix)));
+    return res;
 }
 
 
-function line(start, end) {
+/* Rendering */
+function drawVector(vec, color) {
+    const arrow = [new vec2(-7, 5), new vec2(-7, -5)]
+
+    let centered = relToCenter(vec);
+    line(relToWindow(new vec2(0, 0)), vec, color);
+    if (color !== undefined) {
+        ctx.fillStyle = color;
+    }
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.moveTo(vec.x, vec.y);
+
+    let n = centered.normalize();
+    let rotArrow = multiply(arrow, new Matrix2x2(n.x, -n.y, n.y, n.x));
+    rotArrow[0] = relToWindow(rotArrow[0]);
+    rotArrow[0].x += centered.x;
+    rotArrow[0].y -= centered.y;    
+    rotArrow[1] = relToWindow(rotArrow[1]);
+    rotArrow[1].x += centered.x;
+    rotArrow[1].y -= centered.y;
+
+
+    ctx.lineTo(rotArrow[0].x, rotArrow[0].y);
+    ctx.lineTo(rotArrow[1].x, rotArrow[1].y);
+
+    ctx.fill();
+    ctx.closePath();
+}
+
+function line(start, end, color) {
+    if (color !== undefined) {
+        ctx.strokeStyle = color;
+    }
+    ctx.beginPath();
+    ctx.lineWidth = 2;
     ctx.moveTo(start.x, start.y);
     ctx.lineTo(end.x, end.y);
     ctx.stroke();
+    ctx.closePath();
 }
 
 function dot(position, size) {
@@ -54,6 +103,8 @@ function dot(position, size) {
     ctx.arc(position.x, position.y, size, 0, 2 * Math.PI)
     ctx.fill();
 }
+/* Rendering */
+
 
 window.onload = function () {
     loadContext();
@@ -83,32 +134,42 @@ function applyMatrix(event) {
         new_grid_points[i] = relToWindow(relToCenter(grid_points[i]).multiply(matrix));
     }
     for (let i = 0; i < vectors.length; i++) {
-        new_vectors[i] = vectors[i].multiply(matrix);
+        new_vectors[i] = vectors[i].vec.multiply(matrix);
     }
     let i = 0;
 
-    function int() {
+    function interpolate() {
         for (let i1 = 0; i1 < grid_points.length; i1++) {
             grid_points[i1].x += (new_grid_points[i1].x - grid_points[i1].x) / (100 - i);
             grid_points[i1].y += (new_grid_points[i1].y - grid_points[i1].y) / (100 - i);
         }
         for (let i1 = 0; i1 < vectors.length; i1++) {
-            vectors[i1].x += (new_vectors[i1].x - vectors[i1].x) / (100 - i);
-            vectors[i1].y += (new_vectors[i1].y - vectors[i1].y) / (100 - i);
+            vectors[i1].vec.x += (new_vectors[i1].x - vectors[i1].vec.x) / (100 - i);
+            vectors[i1].vec.y += (new_vectors[i1].y - vectors[i1].vec.y) / (100 - i);
         }
 
         loadContext();
         draw();
         i++;
         if (i < 100)
-            setTimeout(int, 10);
+            setTimeout(interpolate, 10);
     }
-    int();
+    interpolate();
+}
+
+function removeMatrix(event)
+{
+    event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+}
+
+function addMatrix()
+{
+    document.getElementById("matrices").appendChild(document.querySelector(".matrix:first-child").cloneNode(true));
 }
 
 function addVector(event) {
     let inputs = event.target.parentNode.querySelectorAll("input");
-    vectors.push(new vec2(Number(inputs[0].value), Number(inputs[1].value)));
+    vectors.push({ vec: new vec2(Number(inputs[0].value), Number(inputs[1].value)), color: event.target.parentNode.querySelector("input[type=color]").value });
     loadContext();
     draw();
 }
@@ -145,21 +206,19 @@ function loadContext() {
     ctx = canvas.getContext("2d");
 }
 
-var relToCenter = vec => new vec2(vec.x - window.innerWidth / 2, window.innerHeight / 2 - vec.y);
-var relToWindow = vec => new vec2(vec.x + window.innerWidth / 2, window.innerHeight / 2 - vec.y);
 
 function draw() {
     if (show_origin)
         dot(new vec2(window.innerWidth / 2, window.innerHeight / 2), 3);
-        
-    if(show_grid)
-    {
+
+    if (show_grid) {
         for (let i = 0; i < grid_points.length; i += 2) {
             line(grid_points[i], grid_points[i + 1]);
         }
     }
 
     for (vector of vectors) {
-        line(new vec2(window.innerWidth / 2, window.innerHeight / 2), relToWindow(vector));
+        // line(new vec2(window.innerWidth / 2, window.innerHeight / 2), relToWindow(vector.vec), vector.color);
+        drawVector(relToWindow(vector.vec), vector.color);
     }
 }
